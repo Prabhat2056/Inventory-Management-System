b85f22311d83a87ee349f063e855a808ed6bb6f4
Prabhat2056 <prabhat2056@gmail.com>
2024-06-11 14:45:05 +0545

    schema migrate

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,11 @@
-export class Role {}
+import { Role } from "@prisma/client";
+
+export class RoleEntity implements Role {
+    id: number;
+    name: string;
+    created_at: Date;
+    updated_at: Date;
+
+
+
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,36 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RoleEntity } from './entities/role.entity';
 
 @Injectable()
 export class RolesService {
-  constructor(
+  constructor(//dependency injection
     private readonly prismaService: PrismaService,
   ){}
 
-  create(createRoleDto: CreateRoleDto) {
+  async create(createRoleDto: CreateRoleDto): Promise<RoleEntity>{//async ko case ma promise return garnu parxa
     //check if role  already exists
     //if exists throw error Role <name> already exists.
     // if not continue with the request
+    
+    const roleExits = await this.checkIfRoleExistsByName(createRoleDto.name);
+    
+
+    if (roleExits){
+      throw new BadRequestException(`Role ${createRoleDto.name} already exists`);
+    }
     return this.prismaService.role.create({
       data: createRoleDto,
-    })
+    });
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(): Promise<RoleEntity[]> {//[]
+    //@todo: To add Pagiantion
+    return this.prismaService.role.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number): Promise<RoleEntity> {
+    return this.checkIfRoleExists(id);
+
+    
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    await this.checkIfRoleExists(id);
+
+    const roleExists = await this.checkIfRoleExistsByName(updateRoleDto.name, id);
+
+    if (!roleExists) {
+      throw new BadRequestException(`Role ${updateRoleDto.name} already exits.`)
+    }
+
+    return this.prismaService.role
+    .update({
+      where: {
+        id,
+      },
+      data: updateRoleDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    await this.checkIfRoleExists(id);
+    return this.prismaService.role.delete({ where: {id}});
+  }
+
+  private async checkIfRoleExists(id: number): Promise<RoleEntity>{
+    const role = await this.prismaService.role
+      .findFirst({ where: {id}});
+
+    if (!role){
+      throw new NotFoundException();
+    }
+    return role;
+
+  }
+
+  private async checkIfRoleExistsByName(name: string, id?: number): Promise<boolean>{
+    const checkRoleExists = await this.prismaService.role
+    .findUnique({ where: {name}});
+
+    if (id) {
+      return checkRoleExists ? checkRoleExists.id === id : true;
+    }
+    return !!checkRoleExists;
   }
 }
